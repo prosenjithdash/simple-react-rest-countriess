9e95576b28af495b27f98ed76d057f37f05e5704
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-03-13 07:23:39 +0600

    Data props, hold, handle, display done

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -4,6 +4,40 @@ import './Countries.css'
 
 const Countries = () => {
 
+    // 40-5 Conditional CSS And Conditional Style
+    // visited hold state
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    // 40-7 (Recap) Lift Up The State Recap For Visited Flags
+
+    // flage state hold flags data
+    const [visitedFlags, setVisitedFlags] = useState([]);
+
+    // visited handle
+    const handleVisitedCountry = (country) => {
+        console.log('Add visited list.')
+        console.log(country)
+
+        // 40-6 (Advanced) Updating State In Arrays And Pass Parameter
+        // visitedCountries.push(country) // do not work that
+        const newVisitedCountries = [...visitedCountries, country]
+        setVisitedCountries(newVisitedCountries);
+    }
+
+    // 40-7 (Recap) Lift Up The State Recap For Visited Flags
+    // flags handle data
+    const handleVisitedFlag = (flag) => {
+        console.log('Add flag')
+        // console.log(flag)
+
+        const newVisitedFlag = [...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlag);
+        
+    }
+
+
+
+
 
     // Hold data with useState
     const [country, setCountry] = useState([]);
@@ -18,10 +52,39 @@ const Countries = () => {
         <div>
             <h2>Countries: {country.length}</h2>
 
+            {/* 40-6 (Advanced) Updating State In Arrays And Pass Parameter */}
+            {/* Visited countries list */}
+            <div>
+                <h3>Visited Countries: {visitedCountries.length} </h3>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li key={country.cca3}>{ country.name.common}</li> )
+                    }
+                </ul>
+            </div>
+
+             {/* 40-7 (Recap) Lift Up The State Recap For Visited Flags */}
+            {/* Display flag  */}
+            <div className="flag_container">
+                {
+                    visitedFlags.map((flag,idx) => <img key={idx} src={flag} alt="" /> )
+                }
+            </div>
+
             {/* Maping daynamic data. */}
             <div className="countries_container">
                 {
-                    country.map(country => <Country key={country.cca3} country={country}></Country>)
+                    country.map(country =>
+                        <Country
+                        key={country.cca3}
+                        country={country}
+                            
+                         //  40-5 Conditional CSS And Conditional Style with handle props
+                            handleVisitedCountry={handleVisitedCountry}
+                        //  40-7 (Recap) Lift Up The State Recap For Visited Flags 
+                        handleVisitedFlag={handleVisitedFlag}
+                        >  
+                    </Country>)
                 }
             </div>
         </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,26 +1,34 @@
 import { useState } from 'react';
 import './Country.css'
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountry, handleVisitedFlag }) => {
     const { name, flags, population, area, cca3 } = country;
 
     /* 40-4 Toggle State Based On Click And Conditional Rending */
 
     // useState use data hold for visited or not
+    //hold
     const [visited, setVisited] = useState(false)
 
     // eventHandler with toggle useState
+    // handle
     const handleVisited = () => {
         setVisited(!visited);
     }
 
 
+    // console.log(handleVisitedCountry)
+    // console.log(handleVisitedFlag)
+
     return (
-        <div className='country'>
+
+        //40-5 Conditional CSS And Conditional Style
+        // added class when visited
+        <div className={`country ${visited && 'visited'}`}>
 
             {/* 40-2 Display Countries In A Simple Way, Folder Structure */}
-            <img src={flags.png } alt="" />
-            <p>Name:{name?.common}</p>
+            <img className='image_width' src={flags.png } alt="" />
+            <p style={{fontSize:'25px,bold'}}>{name?.common}</p>
             <p>Population:{population}</p>
             <p>Area:{area}</p>
             <p>Code:{cca3}</p>
@@ -28,13 +36,27 @@ const Country = ({ country }) => {
             {/* /* 40-4 Toggle State Based On Click And Conditional Rending */ }
 
             {/* <button onClick={handleVisited}>Visited</button> */}
-            <button onClick={handleVisited}>{visited ? 'Visited': 'Going'}</button>
-
+            {/* Tiger */}
+            <button className='visited_btn' onClick={handleVisited}>{visited ? 'Visited' : 'Visit'}</button> 
+           
 
             {/* Conditional Rending */}
             {
                 visited && 'I have visited this country.'
             }
+
+            <br /> <br />
+
+
+            {/* 40-5 Conditional CSS And Conditional Style */}
+            {/* Mark as Visited */}
+            {/* 40-6 (Advanced) Updating State In Arrays And Pass Parameter */}
+            <button onClick={() => handleVisitedCountry(country)} className='visited_btn'>Mark Visited</button> <br /> <br />
+            
+          
+            <button onClick={() => handleVisitedFlag(country.flags.png)} className='visited_btn'>Add Flag</button>
+
+
         </div>
     );
 };
